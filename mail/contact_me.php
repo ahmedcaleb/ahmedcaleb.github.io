48d7f4e5a6c4e50adbedd6a9c70cac79b394eb50
Caleb <salihucaleb2@gmail.com>
2018-12-29 23:15:10 -0500

    links

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,6 +15,10 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
+
+$mail= new Sendgrid\Mail($from, $subject, $to, $content);
+$apikey = ("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")
+$sg = new \sendgrid($)
 // Create the email and send the message
 $to = 'salihucaleb2@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
@@ -22,5 +26,5 @@ $email_body = "You have received a new message from your website contact form.\n
 $headers = "From: salihucaleb2@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
-header("location: index.html");         
+return true;         
 ?>
